f09d96e83d6161ef911bc23b93d7edb9d554ff51
David Velarde <davidmiguel.velarderobles@kpn.com>
2020-06-28 10:09:10 +0200

    Updated upload docker file

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,18 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=submission
+echo "Docker ID and Image: $dockerpath"
 
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Authenticating..."
+docker login --username=davidvpe
+
+echo "Tagging"
+docker tag $dockerpath davidvpe/udacity_submission:latest
 
 # Step 3:
 # Push image to a docker repository
+echo "Pushing docker image..."
+docker push davidvpe/udacity_submission
