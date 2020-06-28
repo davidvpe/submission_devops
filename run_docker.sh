9a2b2c176811039a7af5101ff35932909c1579a7
David Velarde <davidmiguel.velarderobles@kpn.com>
2020-06-28 09:36:48 +0200

    Dockerfile and run_docker ready

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
+docker build --tag=submission
 
 # Step 1:
 # Build image and add a descriptive tag
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 1000:80 api
